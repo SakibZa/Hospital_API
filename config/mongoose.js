91511c7d438a79c1a405cd3cee6d2d02d4f86e63
sakibhusainzaidi2000@gmail.com <sakibhussain1582000@gmail.com>
2023-02-12 12:25:34 +0530

    All has been commited

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+//connect to DB
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost/hospital_API', { useNewUrlParser: true ,useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to db"));
+
+db.once('open', function () {
+    console.log("Successfully connected to db");
+});
